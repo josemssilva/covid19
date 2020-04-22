b2f47b4f8fb582e6627a62406a25bfff29ab6c73
José Silva <josemssilva@gmail.com>
2020-04-22 08:20:59 +0100

    Improved GlobalCard data

--- src/components/GlobalCard.tsx
+++ src/components/GlobalCard.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import '../App.scss'
 
-const GlobalCard = (props: any) => (
+const GlobalCard = (props: any) => {
+	const currentDate = new Date()
+
+	return (
 	<div className="GlobalCard">
-		<div className="date">{(new Date()).toLocaleDateString()}</div><br/>
+		<div className="date">{currentDate.toLocaleDateString()} @ {currentDate.toLocaleTimeString()}</div><br/>
 		🚑 Confirmed {props.TotalConfirmed} (+{props.NewConfirmed})<br/>
 		⚰️ Deaths {props.TotalDeaths} (+{props.NewDeaths})<br/>
 		💪 Recovered {props.TotalRecovered} (+{props.NewRecovered})
 	</div>
-)
+)}
 
 export default GlobalCard
